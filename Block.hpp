1c73afcc60b7c5e002d5d263fbcdea6a1fb90545
Justus Flerlage <Stazer@live.de>
2014-02-26 18:33:54 +0100

    Blöcke implementiert

--- Block.hpp
+++ Block.hpp
@@ -14,16 +14,32 @@ namespace Tortuga
 				Grass = 2 ,
 				Dirt = 3 ,
 				Cobblestone = 4 ,
-				Wood = 5
+				WoodPlanks = 5
+			} ;
+			
+			enum Metadata
+			{
+				None = 0 ,
+			
+				OakWoodPlanks = 0 ,
+				SpruceWoodPlanks = 1 ,
+				BirchWoodPlanks = 2 ,
+				JungleWoodPlanks = 3 ,
+				AcaciaWoodPlanks = 4 ,
+				DarkOakWoodPlanks = 5
 			} ;
 			
 		private :
 			Type type ;
+			Metadata metadata ;
 			
 		public :
-			Block ( const Tortuga::Block::Type type ) ;
+			Block ( const Tortuga::Block::Type type , const Tortuga::Block::Metadata metadata = Tortuga::Block::None ) ;
 			
 			ARC::Void setType ( const Tortuga::Block::Type type ) ;
 			Tortuga::Block::Type getType ( ) const ;
+			
+			ARC::Void setMetadata ( const Tortuga::Block::Metadata metadata ) ;
+			Tortuga::Block::Metadata getMetadata ( ) const ;
 	} ;
 }
