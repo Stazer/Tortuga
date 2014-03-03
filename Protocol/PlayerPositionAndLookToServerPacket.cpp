990a0eef23aaf2238937368a2422549a889ee310
Justus Flerlage <Stazer@live.de>
2014-03-03 17:20:28 +0100

    PlayerPositionAndLookToServerPacket hinzugefügt

---
+++ Protocol/PlayerPositionAndLookToServerPacket.cpp
@@ -0,0 +1,57 @@
+#include <Tortuga/Protocol/PlayerPositionAndLookToServerPacket.hpp>
+#include <Tortuga/Protocol/PacketReader.hpp>
+#include <Tortuga/Protocol/PacketWriter.hpp>
+
+ARC::Void Tortuga::PlayerPositionAndLookToServerPacket::read ( Tortuga::PacketReader & packetReader )
+{
+	this->location.getPosition ( ).setX ( packetReader.readDouble ( ) ) ;
+	this->location.getPosition ( ).setY ( packetReader.readDouble ( ) ) ; // feet
+	packetReader.readDouble ( ) ; // head
+	this->location.getPosition ( ).setZ ( packetReader.readDouble ( ) ) ;
+	this->location.getOrientation ( ).setX ( packetReader.readFloat ( ) ) ;
+	this->location.getOrientation ( ).setY ( packetReader.readFloat ( ) ) ;
+	this->onGround = packetReader.readBool ( ) ;
+}
+ARC::Void Tortuga::PlayerPositionAndLookToServerPacket::write ( Tortuga::PacketWriter & packetWriter ) const
+{
+	packetWriter.writeVariableInt ( Tortuga::Packet::PlayerPositionAndLookToServer ) ;
+	packetWriter.writeDouble ( this->location.getPosition ( ).getX ( ) ) ;
+	packetWriter.writeDouble ( this->location.getPosition ( ).getY ( ) ) ; // feet
+	packetWriter.writeDouble ( this->location.getPosition ( ).getY ( ) + 1.62 ) ; // head
+	packetWriter.writeDouble ( this->location.getPosition ( ).getZ ( ) ) ;
+	packetWriter.writeFloat ( this->location.getOrientation ( ).getX ( ) ) ;
+	packetWriter.writeFloat ( this->location.getOrientation ( ).getY ( ) ) ;
+	packetWriter.writeBool ( this->onGround ) ;
+}
+
+Tortuga::PlayerPositionAndLookToServerPacket::PlayerPositionAndLookToServerPacket ( Tortuga::PacketReader & packetReader )
+{
+	this->read ( packetReader ) ;
+}
+Tortuga::PlayerPositionAndLookToServerPacket::PlayerPositionAndLookToServerPacket ( const Tortuga::Location & location , const ARC::Bool onGround ) :
+	location ( location ) ,
+	onGround ( onGround )
+{
+}
+
+ARC::Void Tortuga::PlayerPositionAndLookToServerPacket::setLocation ( const Tortuga::Location & location )
+{
+	this->location = location ;
+}
+Tortuga::Location & Tortuga::PlayerPositionAndLookToServerPacket::getLocation ( )
+{
+	return this->location ;
+}
+const Tortuga::Location & Tortuga::PlayerPositionAndLookToServerPacket::getLocation ( ) const
+{
+	return this->location ;
+}
+			
+ARC::Void Tortuga::PlayerPositionAndLookToServerPacket::setOnGround ( const ARC::Bool onGround )
+{
+	this->onGround = onGround ;
+}
+ARC::Bool Tortuga::PlayerPositionAndLookToServerPacket::getOnGround ( ) const
+{
+	return this->onGround ;
+}
---
+++ Protocol/PlayerPositionAndLookToServerPacket.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <ARC.hpp>
+#include <Tortuga/Protocol/Packet.hpp>
+#include <Tortuga/Location.hpp>
+
+namespace Tortuga
+{
+	class PlayerPositionAndLookToServerPacket : public Tortuga::Packet
+	{
+		private :
+			Tortuga::Location location ;
+			ARC::Bool onGround ;
+		
+			ARC::Void read ( Tortuga::PacketReader & packetReader ) ;
+			ARC::Void write ( Tortuga::PacketWriter & packetWriter ) const ;
+			
+		public :
+			PlayerPositionAndLookToServerPacket ( Tortuga::PacketReader & packetReader ) ;
+			PlayerPositionAndLookToServerPacket ( const Tortuga::Location & location , const ARC::Bool onGround ) ;
+			
+			ARC::Void setLocation ( const Tortuga::Location & location ) ;
+			Tortuga::Location & getLocation ( ) ;
+			const Tortuga::Location & getLocation ( ) const ;
+			
+			ARC::Void setOnGround ( const ARC::Bool onGround ) ;
+			ARC::Bool getOnGround ( ) const ;
+	} ;
+}
