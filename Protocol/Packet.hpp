208d103739b328a1effeff47387ca79cbbfdf042
Justus Flerlage <Stazer@live.de>
2014-03-03 17:48:28 +0100

    OperationCodes überarbeitet

--- Protocol/Packet.hpp
+++ Protocol/Packet.hpp
@@ -24,19 +24,22 @@ namespace Tortuga
 			enum OperationCodes
 			{
 				Example = 0x00 ,
+				
+				StatusRequest = 0x00 ,
+				StatusResponse = 0x00 ,
+				StatusKeepAlive = 0x01 ,
 			
 				Handshake = 0x00 ,
 				LoginStart = 0x00 ,
 				LoginSuccess = 0x02 ,
 				KeepAlive = 0x00 ,
+				Disconnect = 0x00 ,
 				
-				StatusRequest = 0x00 ,
-				StatusResponse = 0x00 ,
-				StatusKeepAlive = 0x01 ,
+				ChatMessageFromServer = 0x02 ,
+				ChatMessageToServer = 0x01 ,
 				
 				JoinGame = 0x01 ,
 				ClientSettings = 0x15 ,
-				Disconnect = 0x00 ,
 				
 				SpawnPosition = 0x05 ,
 				TimeUpdate = 0x03 ,
@@ -44,29 +47,11 @@ namespace Tortuga
 				
 				PlayerPositionAndLookFromServer = 0x08 ,
 				PlayerPositionAndLookToServer = 0x06 ,
-			
-				// Client
-				ClientHandshake = 0x00 ,		
-				ClientLoginStart = 0x00 ,
-				ClientLoginSuccess = 0x02 ,
-				ClientKeepAlive = 0x00 ,
-				ClientJoinGame = 0x01 ,
-				ClientDisconnect = 0x00 ,
-				
-				//
-				
-				
-				// Chat
-				ChatMessageFromServer = 0x02 ,
-				ChatMessageToServer = 0x01 ,
-				
-				// World
-				
-				// Player
-				PlayerAbilities = 0x13 ,
+							
+			/*	PlayerAbilities = 0x13 ,
 				PlayerOnGround = 0x03 ,
 				PlayerPosition = 0x04 ,
-				PlayerLook = 0x05
+				PlayerLook = 0x05*/
 				
 			} ;
 	} ;
