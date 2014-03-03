2e16ff58a9ba6e9b675c02da3bfe78d6f66cb6ef
Justus Flerlage <Stazer@live.de>
2014-03-03 17:51:18 +0100

    KeepAlivePacket hinzugefügt

---
+++ Protocol/KeepAlivePacket.cpp
@@ -0,0 +1,30 @@
+#include <Tortuga/Protocol/KeepAlivePacket.hpp>
+#include <Tortuga/Protocol/PacketReader.hpp>
+#include <Tortuga/Protocol/PacketWriter.hpp>
+
+ARC::Void Tortuga::KeepAlivePacket::read ( Tortuga::PacketReader & packetReader )
+{
+	this->identification = packetReader.readInt ( ) ;
+}
+ARC::Void Tortuga::KeepAlivePacket::write ( Tortuga::PacketWriter & packetWriter ) const
+{
+	packetWriter.writeVariableInt ( Tortuga::Packet::KeepAlive ) ;
+	packetWriter.writeInt ( this->identification ) ;
+}
+
+Tortuga::KeepAlivePacket::KeepAlivePacket ( Tortuga::PacketReader & packetReader )
+{
+	this->read ( packetReader ) ;
+}
+Tortuga::KeepAlivePacket::KeepAlivePacket ( const ARC::UnsignedInt identification )
+{
+}
+
+ARC::Void Tortuga::KeepAlivePacket::setIdentification ( const ARC::UnsignedInt identification )
+{
+	this->identification = identification ;
+}
+ARC::UnsignedInt Tortuga::KeepAlivePacket::getIdentification ( ) const
+{
+	return this->identification ;
+}
---
+++ Protocol/KeepAlivePacket.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <ARC.hpp>
+#include <Tortuga/Protocol/Packet.hpp>
+
+namespace Tortuga
+{
+	class KeepAlivePacket : public Tortuga::Packet
+	{
+		private :
+			ARC::UnsignedInt identification ;
+		
+			ARC::Void read ( Tortuga::PacketReader & packetReader ) ;
+			ARC::Void write ( Tortuga::PacketWriter & packetWriter ) const ;
+			
+		public :
+			KeepAlivePacket ( Tortuga::PacketReader & packetReader ) ;
+			KeepAlivePacket ( const ARC::UnsignedInt identification ) ;
+			
+			ARC::Void setIdentification ( const ARC::UnsignedInt identification ) ;
+			ARC::UnsignedInt getIdentification ( ) const ;
+	} ;
+}
